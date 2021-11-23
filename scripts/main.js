258b54edaac1d2c5d8ceda1ec1e7bdbf30533ec5
sihe-gg <85869956+sihe-gg@users.noreply.github.com>
2021-11-23 21:30:11 +0800

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,23 @@
+﻿let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
+function setUserName(){
+	let myName = prompt('请输入你的名字.');
+	if(!myName || myName === null){
+		setUserName();
+	}else{
+		localStorage.setItem('name', myName);
+		myHeading.textContent = '匿名者,' + myName;
+	}
+}
+
+if(!localStorage.getItem('name')){
+	setUserName();
+}else{
+	let storedName = localStorage.getItem('name');
+	myHeading.textContent = '匿名者,' + storedName;
+}
+
+myButton.onclick = function(){
+	setUserName();
+}
